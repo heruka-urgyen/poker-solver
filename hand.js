310c7955623655cb0c049aa552500a0e4a7661aa
urgyen <imalyanov@gmail.com>
2020-06-16 15:53:50 -0500

    solve straight

--- hand.js
+++ hand.js
@@ -7,6 +7,10 @@ const {def, HAND_RANKS, Card, Cards, Hand} = require("./types")
 const sortBy = s => S.sortBy(x => Descending(x[s]))
 const groupBy = s => S.groupBy(x => y => x[s] === y[s])
 
+//    sortRmDup :: Cards -> Cards
+const sortRmDup = def("sortRmDup")({})([Cards, Cards])
+  (cards => S.justs(S.map(S.head)(groupBy("value")(sortBy("value")(cards)))))
+
 //    maybeFlush :: Cards -> Maybe Hand
 const maybeFlush = def("maybeFlush")({})([Cards, $.Maybe(Hand)])
   (cards => S.map
@@ -17,6 +21,30 @@ const maybeFlush = def("maybeFlush")({})([Cards, $.Maybe(Hand)])
         (x => x.length >= 5)
         (groupBy("suit")(sortBy("suit")(cards))))))
 
+//    maybeStraight :: Cards -> Maybe Hand
+const maybeStraight = def("maybeStraight")({})([Cards, $.Maybe(Hand)])
+  (cards => {
+    const uniqSorted = sortRmDup(cards)
+    const ms = S.take(5)
+      (S.reduce
+        (acc => x => {
+          if (acc.length === 5) {return acc}
+
+          if (S.maybe(false)(y => y.value - x.value === 1)(S.last(acc))) {
+            return S.append(x)(acc)
+          }
+
+          return [x]
+        })
+        ([])
+        (uniqSorted))
+
+    const mw = (S.take(5)(S.filter(x => S.elem(x.rank)(["A", "2", "3", "4", "5"]))(uniqSorted)))
+
+    return S.map(cards => ({cards, rank: HAND_RANKS[4]}))(S.isJust(ms)? ms : mw)
+  })
+
 module.exports = {
   maybeFlush,
+  maybeStraight,
 }
--- test/hand.js
+++ test/hand.js
@@ -1,15 +1,15 @@
 const test = require("ava")
 
 const S = require("sanctuary")
-const {maybeFlush} = require ("../hand")
-const {newCard, sortCardsBy} = require ("../card")
+const {maybeFlush, maybeStraight} = require ("../hand")
+const {newCard} = require ("../card")
 
 const highCard = S.map(newCard)(["2c", "4c", "9h", "Ah", "7h", "Jh", "Tc"])
 const pair = S.map(newCard)(["2c", "4c", "2h", "Ah", "7h", "Jh", "Tc"])
 const twoPair = S.map(newCard)(["2c", "4c", "2h", "Ah", "7h", "4d", "Tc"])
 const trips = S.map(newCard)(["2c", "8d", "2h", "Ah", "7h", "2d", "Tc"])
-const straight = S.map(newCard)(["2c", "3d", "6h", "Ah", "4h", "5d", "Tc"])
-const straightWheel = S.map(newCard)(["2c", "3d", "8h", "Ah", "4h", "5d", "Tc"])
+const straight = S.map(newCard)(["2c", "3d", "6h", "4h", "4c", "5d", "Kc"])
+const straightWheel = S.map(newCard)(["2c", "3d", "2h", "Ah", "4h", "5d", "Tc"])
 const flush = S.map(newCard)(["2h", "4c", "9h", "Ah", "7h", "Jh", "Tc"])
 const fullHouse = S.map(newCard)(["2c", "4d", "2h", "Ah", "4h", "2d", "Tc"])
 const quads = S.map(newCard)(["2c", "2d", "2h", "Ah", "4h", "2s", "Tc"])
@@ -86,3 +86,67 @@ test("maybeFlush -> Nothing; quads", t => {
 // })
 //
 //
+
+
+test("maybeStraight -> Nothing; high card", t => {
+  t.deepEqual(
+    maybeStraight(highCard),
+    S.Nothing
+  )
+})
+
+test("maybeStraight -> Nothing; pair", t => {
+  t.deepEqual(
+    maybeStraight(pair),
+    S.Nothing
+  )
+})
+
+test("maybeStraight -> Nothing; two pair", t => {
+  t.deepEqual(
+    maybeStraight(twoPair),
+    S.Nothing
+  )
+})
+
+test("maybeStraight -> Nothing; trips", t => {
+  t.deepEqual(
+    maybeStraight(trips),
+    S.Nothing
+  )
+})
+
+test("maybeStraight -> just straight", t => {
+  t.deepEqual(
+    maybeStraight(straight),
+    S.Just({cards: S.map(newCard)(["6h", "5d", "4h", "3d", "2c"]), rank: "Straight"})
+  )
+})
+
+test("maybeStraight -> just wheel", t => {
+  t.deepEqual(
+    maybeStraight(straightWheel),
+    S.Just({cards: S.map(newCard)(["Ah", "5d", "4h", "3d", "2c"]), rank: "Straight"})
+  )
+})
+
+test("maybeStraight -> Nothing; flush", t => {
+  t.deepEqual(
+    maybeStraight(flush),
+    S.Nothing,
+  )
+})
+
+test("maybeStraight -> Nothing; full house", t => {
+  t.deepEqual(
+    maybeStraight(fullHouse),
+    S.Nothing
+  )
+})
+
+test("maybeStraight -> Nothing; quads", t => {
+  t.deepEqual(
+    maybeStraight(quads),
+    S.Nothing
+  )
+})
